b756f14f54e591ac2881388457a2c1915c97a5a8
googolplex <googolplex@lcompras.biz>
2017-04-30 18:50:56 -0400

    agrego el mapa de transferencia pentaho

--- gitfeliz/grabar_enviar_librocomprasv4.sh
+++ gitfeliz/grabar_enviar_librocomprasv4.sh
@@ -5,9 +5,12 @@ cp /home/xoldfusion/Downloads/openxava-5.6.1_librocomprasv4/tomcat/conf/*.xml li
 cp *.sh librocomprasv4/gitfeliz
 cp -r openshiftfeliz librocomprasv4/
 cp -r pentaho_transformacion librocomprasv4/
-rm -R librocomprasv4/jasperfeliz/*
 # ya no lo hago mas porque esta separado por versiones
-# rm -R jarperfeliz/*.*~
+# rm -R librocomprasv4/jasperfeliz/*
+
+# sigo limpiando los archivos de trabajo nada mas
+rm -R jarperfeliz/*.*~
+rm -R librocomprasv4/jasperfeliz/*.*~
 cp -r jasperfeliz librocomprasv4/
 cp -r tomcatfeliz librocomprasv4/
 cp  amazon2017llave/*.sh librocomprasv4/amazon2017llave
